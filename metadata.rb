c6ec0a94a28ea887892b46359f6a9d2366969e50
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2014-05-28 15:28:37 +0200

    initial commit by chef generate cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'metaltest'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures metaltest'
+long_description 'Installs/Configures metaltest'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: metaltest
+# Recipe:: default
+#
+# Copyright (C) 2014 
+#
+# 
+#
