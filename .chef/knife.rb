9fda11c316251d40a811df283197dcac1d9fc935
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2014-05-29 14:14:13 +0200

    knife, node_name metal-t3stack, add vendor/cookbooks

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,8 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "metal-mastah"
+node_name                "metal-t3stack"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            [::File.join(current_dir, '..', 'cookbooks')]
+cookbook_path            [::File.join(current_dir, '..', 'cookbooks'), ::File.join(current_dir, '..', 'vendor/cookbooks')]
 verify_api_cert          true
