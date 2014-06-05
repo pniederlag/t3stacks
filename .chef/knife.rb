0846cf911034f2bf2952ec0d060c91b947957831
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2014-06-05 11:21:12 +0200

    update knife.rb as suggested by chef-metal/example

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,7 +5,14 @@ node_name                "metal-t3stack"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            [::File.join(current_dir, '..', 'cookbooks'), ::File.join(current_dir, '..', 'vendor/cookbooks')]
-verify_api_cert          true
+#verify_api_cert          true
+
+config_dir "#{File.expand_path('..', __FILE__)}/" # Wherefore art config_dir, chef?
+local_mode true
+begin
+      chef_zero.host '127.0.0.1'
+rescue
+end
 
 # profiles for chef-metal
 #profile 'xxxefault'
