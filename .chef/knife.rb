1433c5c865a1ab742453a879bc337fb9c27fc4b2
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2014-06-11 11:51:35 +0200

    knife, add explicit data_bag path

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,6 +5,7 @@ node_name                "metal-t3stack"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            [::File.join(current_dir, '..', 'cookbooks'), ::File.join(current_dir, '..', 'vendor/cookbooks')]
+data_bag_path            ::File.join(current_dir, '..', 'data_bags')
 #verify_api_cert          true
 
 config_dir "#{File.expand_path('..', __FILE__)}/" # Wherefore art config_dir, chef?
