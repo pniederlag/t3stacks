b0560ee222f5f21084ff1e8df6d7638fd52aca34
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2014-06-11 11:53:17 +0200

    knife.rb, add some comments

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,6 +9,14 @@ data_bag_path            ::File.join(current_dir, '..', 'data_bags')
 #verify_api_cert          true
 
 config_dir "#{File.expand_path('..', __FILE__)}/" # Wherefore art config_dir, chef?
+
+# these settings could be used to work against a "real" chef server,
+# keep in mind, you must set local_mode = false below
+#chef_server_url "http://127.0.0.1:8889
+#client_key current_dir + "/fixme-to-some.pem"
+
+# local_mode enables automatically firing up an "internal" chef_zero
+# it will persist all data into nodes, data_bags on disk(!)
 local_mode true
 begin
       chef_zero.host '127.0.0.1'
